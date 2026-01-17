ea5f498646c39f639b736b97f02ffb5baae6025b
Christoph Feuerer <helightdev@gmail.com>
2026-01-17 18:12:00 +0100

    feat: Add appendProperty codec extension

---
+++ mods/kotale/src/main/kotlin/dev/helight/kotale/ext/CodecExtensions.kt
@@ -0,0 +1,26 @@
+package dev.helight.kotale.ext
+
+import com.hypixel.hytale.codec.Codec
+import com.hypixel.hytale.codec.KeyedCodec
+import com.hypixel.hytale.codec.builder.BuilderCodec
+import com.hypixel.hytale.codec.builder.BuilderField
+import java.lang.invoke.MethodHandles
+import kotlin.reflect.KMutableProperty
+import kotlin.reflect.jvm.javaMethod
+
+fun <SELF, T, BUILDER : BuilderCodec.BuilderBase<SELF, BUILDER>> BUILDER.appendProperty(
+    property: KMutableProperty<T>, codec: Codec<T>,
+    name: String = property.name,
+    required: Boolean = false
+): BuilderField.FieldBuilder<SELF, T, BUILDER> {
+    val lookup = MethodHandles.lookup()
+    val getter = lookup.unreflect(property.getter.javaMethod)
+    val setter = lookup.unreflect(property.setter.javaMethod)
+
+    @Suppress("UNCHECKED_CAST")
+    return this.append(
+        KeyedCodec<T>(name, codec, required),
+        { comp: SELF, value: T -> setter.invokeExact(comp, value) },
+        { comp: SELF -> getter.invokeExact(comp) as T }
+    )
+}
